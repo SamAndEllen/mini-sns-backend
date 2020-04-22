0078744effa083ca3fa9b2219aa19f6ffc125946
jo-narae <nare6004@gmail.com>
2020-04-22 16:37:50 +0900

    example joi

--- src/api/members/index.js
+++ src/api/members/index.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router');
 const memberAPI = new Router();
 
+const Joi = require('joi');
+
 memberAPI.get('/', async (ctx, next) => {
     const [result] = await ctx.state.db.query('SELECT * FROM members_test');
     ctx.body = result;
@@ -12,19 +14,19 @@ memberAPI.post('/', async (ctx, next) => {
     const [IDCheck] = await ctx.state.db.query(`
         SELECT * FROM members_test WHERE email = '${param.email}'
     `);
-    
-    let errorMessage = '일부 입력되지 않았습니다.';
-    if (IDCheck.length > 0) errorMessage = '이메일이 중복됩니다.';
 
-    if (
-        param.name === undefined || param.email === undefined || param.password === undefined
-        || param.agreeTOS === undefined || param.agreePrivacy === undefined || IDCheck.length > 0
-    ) {
+    const schema = Joi.object().keys({
+        name: Joi.string().alphanum().min(3).max(10).required(),
+        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        password: Joi.string().required(),
+        agreeTOS: Joi.boolean().required(),
+        agreePrivacy: Joi.boolean().required()
+    });
+
+    const paramCheck = Joi.validate(param, schema);
+
+    if (paramCheck.error || IDCheck.length > 0) {
         ctx.status = 400;
-        ctx.body = {
-            result: false,
-            errorMessage
-        };
         return;
     }
 
